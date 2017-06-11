d7beda8f0644b0d41c8f05e44db9c01dd49ce994
Unknown <m.vehar@gmail.com>
2017-06-11 11:23:22 +0200

    Fix 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,13 @@
 
 adduser -D ${USER} && echo "${USER}:${PASS}" | chpasswd
 
+ssh-keygen -A
+
 sed -i s/[#]*PasswordAuthentication.*/PasswordAuthentication\ yes/ /etc/ssh/sshd_config 
 sed -i s/[#]*PubkeyAuthentication.*/PubkeyAuthentication\ yes/ /etc/ssh/sshd_config
 sed -i s/[#]*AllowTcpForwarding.*/AllowTcpForwarding\ yes/ /etc/ssh/sshd_config
 sed -i s/[#]*GatewayPorts.*/GatewayPorts\ yes/ /etc/ssh/sshd_config
 sed -i s/[#]*ClientAliveInterval.*/ClientAliveInterval\ 15/ /etc/ssh/sshd_config
 
-ssh-keygen -A
 
 /usr/sbin/sshd -D -e $@
